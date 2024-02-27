f59e6a90733f38efdcf80d1c930afc612c619059
t1d3nio <t1d3nio@gmail.com>
2024-02-27 11:10:14 -0800

    fixing teamspeak timeout

--- brute/teamspeak.go
+++ brute/teamspeak.go
@@ -10,35 +10,20 @@ import (
 func BruteTeamSpeak(host string, port int, user, password string, timeout time.Duration) (bool, bool) {
 	portstr := strconv.Itoa(port)
 	hoststr := host + ":" + portstr
-	c, err := ts3.NewClient(hoststr)
+	c, err := ts3.NewClient(hoststr, ts3.Timeout(timeout))
 	if err != nil {
-		_ = err
 		return false, false
 	}
 	defer c.Close()
 
-	timeoutChan := time.After(timeout)
-
-	done := make(chan struct{})
-	defer close(done)
-
-	go func() {
-		select {
-		case <-done:
-			return
-		case <-timeoutChan:
-		}
-	}()
-
-	if err := c.Login(user, password); err != nil {
-		_ = err
+	err = c.Login(user, password)
+	if err != nil {
 		return false, true
 	}
 
 	if _, err := c.Version(); err != nil {
-		_ = err
 		return false, true
-	} else {
-		return true, true
 	}
+
+	return true, true
 }
