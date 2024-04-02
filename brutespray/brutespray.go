6d848fe7d15bdc1b26849fde16ea0573d0fb3164
t1d3nio <t1d3nio@gmail.com>
2024-04-02 17:46:45 -0500

    working on rdp

--- brutespray/brutespray.go
+++ brutespray/brutespray.go
@@ -17,7 +17,7 @@ import (
 	"github.com/x90skysn3k/brutespray/modules"
 )
 
-var masterServiceList = []string{"ssh", "ftp", "smtp", "mssql", "telnet", "smbnt", "postgres", "imap", "pop3", "snmp", "mysql", "vmauthd", "asterisk", "vnc", "mongodb", "nntp", "oracle", "teamspeak", "xmpp"}
+var masterServiceList = []string{"ssh", "ftp", "smtp", "mssql", "telnet", "smbnt", "postgres", "imap", "pop3", "snmp", "mysql", "vmauthd", "asterisk", "vnc", "mongodb", "nntp", "oracle", "teamspeak", "xmpp", "rdp"}
 
 var BetaServiceList = []string{"asterisk", "nntp", "oracle", "xmpp"}
 
