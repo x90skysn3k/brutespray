ad9209789ffc30c116c4b2cc05ba27e2a23d6b6e
t1d3nio <t1d3nio@gmail.com>
2024-04-02 17:30:44 -0500

    working on rdp

--- brute/rdp.go
+++ brute/rdp.go
@@ -2,23 +2,8 @@ package brute
 
 import (
 	"fmt"
-	"time"
-)
-
-func BruteRDP(host string, port int, user, password string, timeout time.Duration) (bool, bool) {
-	fmt.Println("not needed")
-	return false, false
-}
-
-/*
-package brute
-
-import (
-	"context"
-	"fmt"
-	"log"
 	"net"
-	"os"
+	"sync"
 	"time"
 
 	"github.com/tomatome/grdp/core"
@@ -32,105 +17,53 @@ import (
 )
 
 func BruteRDP(host string, port int, user, password string, timeout time.Duration) (bool, bool) {
-	domain := ".\\"
-	width := 600
-	height := 600
-
-	target := fmt.Sprintf("%s:%d", host, port)
-
-	glog.SetLevel(glog.INFO)
-	logger := log.New(os.Stdout, "", 0)
-	glog.SetLogger(logger)
-
-	client := NewRdpClient(target, width, height, glog.INFO, user, password, domain)
-
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-
-	type result struct {
-		success bool
-		err     error
-	}
-	done := make(chan result)
-
-	go func() {
-		err := client.Login()
-		success := err == nil
-		done <- result{success, err}
-		client.Close()
-	}()
-
-	select {
-	case <-ctx.Done():
-		return false, false
-	case res := <-done:
-		if res.err != nil {
-			return false, true
-		}
-		return true, true
-	}
-}
-
-type RdpClient struct {
-	Host     string // ip:port
-	Width    int
-	Height   int
-	user     string
-	password string
-	domain   string
-	tpkt     *tpkt.TPKT
-	x224     *x224.X224
-	mcs      *t125.MCSClient
-	sec      *sec.Client
-	pdu      *pdu.Client
-}
-
-func NewRdpClient(host string, width, height int, logLevel glog.LEVEL, user, password, domain string) *RdpClient {
-	return &RdpClient{
-		Host:     host,
-		Width:    width,
-		Height:   height,
-		user:     user,
-		password: password,
-		domain:   domain,
-	}
-}
-
-func (g *RdpClient) Login() error {
-	conn, err := net.DialTimeout("tcp", g.Host, 3*time.Second)
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), timeout)
 	if err != nil {
-		return fmt.Errorf("[dial err] %v", err)
+		glog.Errorf("[dial err] %v", err)
+		return false, false // Connection failed
 	}
 	defer conn.Close()
+	glog.Info(conn.LocalAddr().String())
 
-	g.tpkt = tpkt.New(core.NewSocketLayer(conn), nla.NewNTLMv2(g.domain, g.user, g.password))
-	g.x224 = x224.New(g.tpkt)
+	tpkt := tpkt.New(core.NewSocketLayer(conn), nla.NewNTLMv2("", user, password))
+	x224 := x224.New(tpkt)
+	mcs := t125.NewMCSClient(x224)
+	sec := sec.NewClient(mcs)
+	pdu := pdu.NewClient(sec)
 
-	g.mcs = t125.NewMCSClient(g.x224)
-	g.sec = sec.NewClient(g.mcs)
-	g.pdu = pdu.NewClient(g.sec)
+	sec.SetUser(user)
+	sec.SetPwd(password)
 
-	//g.mcs.SetClientDesktop(uint16(g.Width), uint16(g.Height))
-	g.sec.SetUser(g.user)
-	g.sec.SetPwd(g.password)
-	g.sec.SetDomain(g.domain)
+	tpkt.SetFastPathListener(sec)
+	sec.SetFastPathListener(pdu)
+	pdu.SetFastPathSender(tpkt)
 
-	g.tpkt.SetFastPathListener(g.sec)
-	g.sec.SetFastPathListener(g.pdu)
-	g.sec.SetChannelSender(g.mcs)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-	g.x224.SetRequestedProtocol(x224.PROTOCOL_SSL)
-
-	err = g.x224.Connect()
+	err = x224.Connect()
 	if err != nil {
-		return fmt.Errorf("[x224 connect err] %v", err)
+		glog.Errorf("[x224 connect err] %v", err)
+		return false, false
 	}
-	return nil
-}
 
-func (g *RdpClient) Close() {
-	if g != nil && g.tpkt != nil {
-		g.tpkt.Close()
-	}
+	pdu.On("error", func(e error) {
+		glog.Error("error", e)
+		wg.Done()
+	})
+	pdu.On("close", func() {
+		glog.Info("on close")
+		wg.Done()
+	})
+	pdu.On("success", func() {
+		glog.Info("on success")
+		wg.Done()
+	})
+	pdu.On("ready", func() {
+		glog.Info("on ready")
+		wg.Done()
+	})
+
+	wg.Wait()
+	return true, true // Return authentication status
 }
-*/
