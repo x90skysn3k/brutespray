971b21f3be5e242fc808050779ad7e18b4b2e7f4
t1d3nio <t1d3nio@gmail.com>
2024-12-05 13:46:31 -0600

    implementing a connection manager

--- brute/run.go
+++ brute/run.go
@@ -53,7 +53,7 @@ func RunBrute(h modules.Host, u string, p string, progressCh chan<- int, timeout
 		case "teamspeak":
 			result, con_result = BruteTeamSpeak(h.Host, h.Port, u, p, timeout, socks5)
 		case "xmpp":
-			result, con_result = BruteXMPP(h.Host, h.Port, u, p, timeout)
+			result, con_result = BruteXMPP(h.Host, h.Port, u, p, timeout, socks5)
 		case "rdp":
 			result, con_result = BruteRDP(h.Host, h.Port, u, p, timeout, socks5)
 		default:
