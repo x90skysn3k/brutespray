aaf17ee2b89ba11d75e0d0334e048b8509bb3c5b
t1d3nio <t1d3nio@gmail.com>
2025-01-09 10:59:54 -0600

    initial attempt at capping retries

--- brute/run.go
+++ brute/run.go
@@ -8,106 +8,81 @@ import (
 	"github.com/x90skysn3k/brutespray/modules"
 )
 
-var retryMap = make(map[string]int)
-var skipMap = make(map[string]bool)
-var skippedMap = make(map[string]bool)
-var retryMapMutex = &sync.Mutex{}
+type retryInfo struct {
+	key           string
+	retries       int
+	maxRetries    int
+	skip          bool
+	skipped       bool
+	retrying      bool
+	retryMapMutex *sync.Mutex
+}
 
 func ClearMaps() {
-	retryMap = make(map[string]int)
-	skipMap = make(map[string]bool)
-	skippedMap = make(map[string]bool)
+	retryInfoMap = make(map[string]*retryInfo)
+}
+
+var retryInfoMap = make(map[string]*retryInfo)
+
+func getRetryInfo(h modules.Host, maxRetries int) *retryInfo {
+	key := h.Host + ":" + h.Service
+
+	retryInfoMap[key].retryMapMutex.Lock()
+	defer retryInfoMap[key].retryMapMutex.Unlock()
+
+	if retryInfoMap[key] == nil {
+		retryInfoMap[key] = &retryInfo{
+			key:           key,
+			retries:       0,
+			maxRetries:    maxRetries,
+			skip:          false,
+			skipped:       false,
+			retrying:      false,
+			retryMapMutex: &sync.Mutex{},
+		}
+	}
+	return retryInfoMap[key]
 }
 
 func RunBrute(h modules.Host, u string, p string, progressCh chan<- int, timeout time.Duration, maxRetries int, output string, socks5 string, netInterface string) bool {
 	service := h.Service
+	retryInfo := getRetryInfo(h, maxRetries)
 	var result bool
 	var con_result bool
-	var retrying bool = false
 	var delayTime time.Duration = 1 * time.Second
 
-	key := h.Host + ":" + h.Service
-
 	for {
-		retryMapMutex.Lock()
-		retries, ok := retryMap[key]
-		if !ok && !retrying {
-			retries = 0
-		}
-		if retries >= maxRetries && !skipMap[key] {
-			skipMap[key] = true
+		retryInfo.retryMapMutex.Lock()
+		retries := retryInfo.retries
+		if retries >= maxRetries && !retryInfo.skip {
+			retryInfo.skip = true
 			modules.PrintSkipping(h.Host, service, retries, maxRetries)
-			skippedMap[key] = true
-			retryMapMutex.Unlock()
+			retryInfo.skipped = true
+			retryInfo.retryMapMutex.Unlock()
 			return false
 		}
 		retries++
-		retryMap[key] = retries
-		retryMapMutex.Unlock()
+		retryInfo.retries = retries
+		retryInfo.retryMapMutex.Unlock()
 
-		if skippedMap[key] {
+		if retryInfo.skipped {
 			return false
 		}
 
 		switch service {
-		case "ssh":
-			result, con_result = BruteSSH(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "ftp":
-			result, con_result = BruteFTP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "mssql":
-			result, con_result = BruteMSSQL(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "telnet":
-			result, con_result = BruteTelnet(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "smbnt":
-			result, con_result = BruteSMB(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "postgres":
-			result, con_result = BrutePostgres(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "smtp":
-			result, con_result = BruteSMTP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "imap":
-			result, con_result = BruteIMAP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "pop3":
-			result, con_result = BrutePOP3(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "snmp":
-			result, con_result = BruteSNMP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "mysql":
-			result, con_result = BruteMYSQL(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "vmauthd":
-			result, con_result = BruteVMAuthd(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "asterisk":
-			result, con_result = BruteAsterisk(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "vnc":
-			result, con_result = BruteVNC(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "mongodb":
-			result, con_result = BruteMongoDB(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "nntp":
-			result, con_result = BruteNNTP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "oracle":
-			result, con_result = BruteOracle(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "teamspeak":
-			result, con_result = BruteTeamSpeak(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "xmpp":
-			result, con_result = BruteXMPP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
-		case "rdp":
-			result, con_result = BruteRDP(h.Host, h.Port, u, p, timeout, socks5, netInterface)
+		// Bruteforce functions go here
 		default:
 			return con_result
 		}
 		if con_result {
-			//fmt.Println(con_result, retries)
-			retryMapMutex.Lock()
-			retries--
-			retryMap[key] = retries
-			retryMapMutex.Unlock()
 			break
 		} else {
 			delayTime = time.Duration(int64(time.Second) * int64(math.Min(float64(retries), float64(delayTime))))
-			//fmt.Println(con_result, retries)
-			retrying := true
-			modules.PrintResult(service, h.Host, h.Port, u, p, result, con_result, progressCh, retrying, output, delayTime)
+			retryInfo.retrying = true
+			modules.PrintResult(service, h.Host, h.Port, u, p, result, con_result, progressCh, retryInfo.retrying, output, delayTime)
 			time.Sleep(delayTime)
 		}
 	}
-	modules.PrintResult(service, h.Host, h.Port, u, p, result, con_result, progressCh, retrying, output, delayTime)
+	modules.PrintResult(service, h.Host, h.Port, u, p, result, con_result, progressCh, retryInfo.retrying, output, delayTime)
 	return con_result
 }
