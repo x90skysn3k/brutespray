9a4ba837a4f309f1f9ba1f411585fdb5c7c89f00
x90skysn3k <t1d3nio@gmail.com>
2026-05-29 09:57:18 -0500

    feat(brute): influxdb v2 token + v1 basic-auth module

--- wordlist/embed.go
+++ wordlist/embed.go
@@ -2,5 +2,5 @@ package wordlist
 
 import "embed"
 
-//go:embed manifest.yaml all:_base all:_layers all:overrides snmp couchdb elasticsearch
+//go:embed manifest.yaml all:_base all:_layers all:overrides snmp couchdb elasticsearch influxdb
 var FS embed.FS
